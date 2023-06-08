c378ca6cb87976da9e342503f6efcadd9621286d
Anders-RM <Anders_RMathiesen@pm.me>
2023-06-08 11:38:25 +0200

    test

---
+++ win 11.ps1
@@ -0,0 +1,21 @@
+# Define variables
+$vmName = "Windows 11"
+$vmMemory = 4096
+$vmProcessorCount = 2
+$vmDiskPath = "C:\VMs\Windows 11\Virtual Hard Disks\Windows 11.vhdx"
+$isoPath = "C:\ISOs\Windows 11.iso"
+
+# Create a new virtual machine
+New-VM -Name $vmName -MemoryStartupBytes $vmMemory -SwitchName "Default" -Path "C:\VMs"
+
+# Set the number of virtual processors
+Set-VMProcessor -VMName $vmName -Count $vmProcessorCount
+
+# Add a virtual hard disk
+Add-VMHardDiskDrive -VMName $vmName -Path $vmDiskPath
+
+# Attach the ISO file
+Add-VMDvdDrive -VMName $vmName -Path $isoPath
+
+# Start the virtual machine
+Start-VM -Name $vmName
