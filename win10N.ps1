3edc03102719f4a46172f66ec8479277bbaf182d
Anders-RM <Anders_RMathiesen@pm.me>
2023-07-04 22:58:54 +0200

    set up for win10 set up for win10

---
+++ win10N.ps1
@@ -0,0 +1,36 @@
+# Run ExternalSwitch.ps1 script to configure the network switch
+if (-not (Get-VMSwitch -Name "ExternalSwitch")) {
+    # Create external switch
+    & "$PSScriptRoot/ExternalSwitch.ps1"
+}
+
+# Define variables for the virtual machine
+$vmName = "Windows10"
+$vmMemory = 4096MB
+$vmProcessorCount = 2
+$vmDiskPath = "C:\VMs\Windows11\VHD\Windows11.vhdx"
+$isoPath = "C:\ISOs\Windows10.iso"
+$switchName = "ExternalSwitch"
+$vhdSize = 127GB
+
+# Create an empty VHDX file for the virtual machine
+New-VHD -Path $vmDiskPath -SizeBytes $vhdSize -Dynamic
+
+# Create a new virtual machine with the specified settings
+New-VM -Name $vmName -Generation 1 -MemoryStartupBytes $vmMemory -SwitchName $switchName -Path "C:\VMs"
+
+# Set the number of virtual processors for the virtual machine
+Set-VMProcessor -VMName $vmName -Count $vmProcessorCount
+
+# Add a virtual hard disk to the virtual machine
+Add-VMHardDiskDrive -VMName $vmName -Path $vmDiskPath
+
+# Attach the ISO file to the virtual machine
+Add-VMDvdDrive -VMName $vmName -Path $isoPath
+
+Set-VM -Name $VMName -AutomaticCheckpointsEnabled $false
+
+Set-VMFirmware -VMName $VMName -FirstBootDevice (Get-VMDvdDrive -VMName $VMName)
+
+# Start the virtual machine
+Start-VM -Name $vmName
---
+++ win10VHD.ps1
@@ -0,0 +1,33 @@
+# Run ExternalSwitch.ps1 script to configure the network switch
+if (-not (Get-VMSwitch -Name "ExternalSwitch")) {
+    # Create external switch
+    & "$PSScriptRoot/ExternalSwitch.ps1"
+}
+
+# Define variables for the virtual machine
+$vmName = "Windows10"
+$vmMemory = 4096MB
+$vmProcessorCount = 2
+$vmDiskPath = "C:\VMs\Windows11\VHD\Windows10.vhdx"
+$switchName = "ExternalSwitch"
+
+Copy-Item .\Windows11.vhdx $vmDiskPath
+
+# Create a new virtual machine with the specified settings
+New-VM -Name $vmName -Generation 2 -MemoryStartupBytes $vmMemory -SwitchName $switchName -Path "C:\VMs"
+
+# Set the number of virtual processors for the virtual machine
+Set-VMProcessor -VMName $vmName -Count $vmProcessorCount
+
+# Add a virtual hard disk to the virtual machine
+Add-VMHardDiskDrive -VMName $vmName -Path $vmDiskPath
+
+Set-VM -Name $VMName -AutomaticCheckpointsEnabled $false
+
+Set-VMFirmware -VMName $VMName -FirstBootDevice (Get-VMHardDiskDrive -VMName $VMName)
+
+# Start the virtual machine
+Start-VM -Name $vmName
+
+
+
